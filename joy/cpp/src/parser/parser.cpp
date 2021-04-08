3ada553ebb6afd3f23e0e211a7d4939f15cd5f2c
karmveer <catchkvs>
2021-04-08 18:31:14 -0400

    fixing the build issues
    Former-commit-id: e34b9dcc161adb4345bb6bcf172d540170940fcf

--- joy/cpp/src/parser/parser.cpp
+++ joy/cpp/src/parser/parser.cpp
@@ -1,126 +1,190 @@
 #include <stdio.h>
 #include <iostream>
 #include <cstring>
-#include <stack>          // std::stack
+#include <stack> // std::stack
 using namespace std;
 
-enum LogicalOperator { AND, OR };
-enum ComparisionOperator { EQ, NEQ, LT, LTE, GT, GTE};
-class Expr {
+enum LogicalOperator
+{
+    AND,
+    OR
+};
+enum ComparisionOperator
+{
+    EQ,
+    NEQ,
+    LT,
+    LTE,
+    GT,
+    GTE
+};
+class Expr
+{
+};
+
+class BinaryExpr : public Expr
+{
+public:
+    LogicalOperator op;
+    Expr left;
+    Expr right;
+};
+
+class ComparisionExpr : public Expr
+{
+public:
+    ComparisionOperator op;
+    int columnIdx;
+    int columnData;
 
 };
 
-class BinaryExpr: public Expr {
-    public:
-        LogicalOperator op;
-        Expr left;
-        Expr right;
-};
-
-class ComparisionExpr: public Expr {
-    public:
-        ComparisionOperator op;
-        int columnIdx;
-        int columnData;
-
-}
-
-class Parser {
+class Parser
+{
 
 public:
+    Expr parseRowExpression(string input);
+    ComparisionExpr generateComparisionExpr(string exprStr, int startIdx, int endIdx);
+};
 
-    Expr parseRowExpression(string input) {
+Expr Parser::parseRowExpression(string input)
+    {
         int n = input.length();
         BinaryExpr root;
         // declaring character array
         char char_array[n + 1];
         bool shouldBeRight = false;
- 
-    // copying the contents of the
-    // string to char array
+
+        // copying the contents of the
+        // string to char array
         strcpy(char_array, input.c_str());
         stack<BinaryExpr> exprStack;
         printf("inside parser:::");
         int lastIdx = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             char ch = char_array[i];
-            switch(ch) {
-                case '(':
-                    string opStr = input.substr(lastIdx, i);
-                    if(opStr == "AND") {
-                        BinaryExpr expr;
-                        expr.op = LogicalOperator::AND;
-                        root = expr;
-                        exprStack.push(expr);
-                        lastIdx = i+1;
-                    } else if(opStr.find("operator") == string::npos) {
+            switch (ch)
+            {
+            case '(':
+            {
+                string opStr = input.substr(lastIdx, i);
+                if (opStr == "AND")
+                {
+                    BinaryExpr expr;
+                    expr.op = LogicalOperator::AND;
+                    root = expr;
+                    exprStack.push(expr);
+                    lastIdx = i + 1;
+                }
+                else if (opStr.find("operator") == string::npos)
+                {
+                    string op = opStr.substr(10);
+                    if (op == "EQ" || op == "LT" || op == "GT" || op == "LTE" || op == "GTE")
+                    {
+                        i = i + 1;
+                        ch = char_array[i];
+                        int startIdx = i;
+                        string result;
+                        while (ch != ')')
+                        {
+                            result = result.append("" + ch);
+                            i++;
+                            ch = char_array[i];
+                        }
+
+                        ComparisionExpr expr = this->generateComparisionExpr(opStr, startIdx, i);
+                        return expr;
+                    }
+                }
+                else
+                {
+                    string opStr = opStr.substr(lastIdx, i);
+                    exprStack.pop();
+                    BinaryExpr parent = (BinaryExpr)exprStack.top();
+                    if (opStr == "AND")
+                    {
+                        if (shouldBeRight)
+                        {
+                            BinaryExpr newExpr;
+
+                            parent.right = newExpr;
+                            exprStack.push(parent);
+                            exprStack.push(newExpr);
+                            shouldBeRight = false;
+                        }
+                        else
+                        {
+                            BinaryExpr newExpr;
+
+                            parent.left = newExpr;
+                            exprStack.push(parent);
+                            exprStack.push(newExpr);
+                            shouldBeRight = false;
+                        }
+                    }
+                    else if (opStr.find("operator") > 0)
+                    {
                         string op = opStr.substr(10);
-                        if (op == "EQ" || op == "LT" || op == "GT" || op == "LTE" || op == "GTE") {
+                        if (op == "EQ" || op == "LT" || op == "GT" || op == "LTE" || op == "GTE")
+                        {
                             i = i + 1;
                             ch = char_array[i];
                             int startIdx = i;
                             string result;
-                            while ( ch != ')') {
+                            while (ch != ')')
+                            {
                                 result = result.append("" + ch);
                                 i++;
                                 ch = char_array[i];
                             }
-                            
-                            ComparisionExpr expr = generateComparisionExpr(opStr, startIdx, i);
-                            return expr;
-                            
-                        }
-                    } else {
-                        string opStr = opStr.substr(lastIdx, i);
 
-                        BinaryExpr parent = (BinaryExpr) exprStack.pop();
-                        if(opStr == "AND") {
-                            if(shouldBeRight) {
-                                BinaryExpr newExpr;
-
-                                parent.right = newExpr;
+                            ComparisionExpr cmpExpr = this->generateComparisionExpr(opStr, startIdx, i);
+                            if (shouldBeRight)
+                            {
+                                parent.right = cmpExpr;
                                 exprStack.push(parent);
-                                exprStack.push(newExpr);
-                                shouldBeRight = false;
-                            } else {
-                                BinaryExpr newExpr;
-
-                                parent.left = newExpr;
-                                exprStack.push(parent);
-                                exprStack.push(newExpr);
                                 shouldBeRight = false;
                             }
-
-                        } else if(opStr.find("operator") > 0) {
-                            string op = opStr.substr(10);
-                            if (op == "EQ" || op == "LT" || op == "GT" || op == "LTE" || op == "GTE") {
-                                i = i + 1;
-                                ch = char_array[i];
-                                int startIdx = i;
-                                string result;
-                                while ( ch != ')') {
-                                    result = result.append("" + ch);
-                                    i++;
-                                    ch = char_array[i];
-                                }
-                                
-                                ComparisionExpr expr = generateComparisionExpr(opStr, startIdx, i);
-                                return expr;
-                                
+                            else
+                            {
+                                parent.left = cmpExpr;
+                                exprStack.push(parent);
+                                shouldBeRight = false;
                             }
                         }
+                    }
                     break;
-                case ')':
-                    break;
-                case ' ':
-                    break;
-                case ',':
-                    break;         
+                }
+            }
+
+            case ')':
+            {
+                if (!exprStack.empty())
+                {
+                    exprStack.pop();
+                }
+                break;
+            }
+
+            case ' ':
+            {
+                lastIdx++;
+                break;
+            }
+            case ',':
+            {
+                shouldBeRight = true;
+                break;
+            }
+            default:
+                break;
             }
         }
     }
 
-    ComparisionExpr generateComparisionExpr(string exprStr, int startIdx, int endIdx) {
+    ComparisionExpr Parser::generateComparisionExpr(string exprStr, int startIdx, int endIdx)
+    {
         string cmprStr = exprStr.substr(startIdx, endIdx);
         int position = cmprStr.find_first_of(",");
         string fieldIdx = cmprStr.substr(0, position);
@@ -131,11 +195,8 @@ public:
         expr.columnData = stoi(fieldData);
         return expr;
     }
-
-};
-
-
-int main() {
+int main()
+{
     Parser parserObj;
 
     parserObj.parseRowExpression("AND(AND($operator$GT(#3, 8766), $operator$LT(#3, 9131)), AND(BETWEEN(#2, 0.05, 0.07), $operator$LT(#0, 24.0)))");
