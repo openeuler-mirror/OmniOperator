1e9f4d05ebc293235b3400c1bb53bbc4564b3624
karmveer <catchkvs>
2021-04-28 14:13:48 -0400

    basic interface for llvm code gen
    Former-commit-id: e6a18aa6c241156bee213ef9e201b31d7ccf9809

---
+++ joy/cpp/src/codegen/llvm_codegen.h
@@ -0,0 +1,27 @@
+#ifndef __LLVM_CODEGEN_H__
+#define __LLVM_CODEGEN_H__
+
+#include "../common/expressions.h"
+#include <cstring>
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
+class LLVMCodeGen
+{
+public:
+    llvm::Function *generateFunc(Expr exp);
+    
+};
+
+int simpleTest();
+
+#endif
