1019cd538b7a4bab2b0036f183f8b2a9bc1d3844
songling-olk <liusongling1@huawei.com>
2021-05-08 10:09:11 +0800

    Add parent class
    Former-commit-id: 7887620fa2ca28b6620e0775833a7ccfb6298949

---
+++ core/src/operator/native_base.h
@@ -0,0 +1,43 @@
+#ifndef __NATIVE_BASE_H__
+#define __NATIVE_BASE_H__
+
+#include "../vector/table.h"
+#include <vector>
+
+typedef struct JitContext
+{
+    uintptr_t jitter;
+    uintptr_t func;
+} JitContext;
+
+class NativeOmniOperatorFactory 
+{
+public:
+    NativeOmniOperatorFactory() {}
+    virtual ~NativeOmniOperatorFactory(){}
+    virtual uintptr_t createOmniOperator(){};
+    virtual void setJitContext(JitContext* JitContext)
+    {
+        jitContext = JitContext;
+    }
+    virtual JitContext* getJitContext()
+    {
+        return jitContext;
+    }
+private:
+    JitContext* jitContext;
+};
+
+class NativeOmniOperator
+{
+public:
+    NativeOmniOperator() {}
+    virtual ~NativeOmniOperator(){}
+    // TBD addInput return ErrNo 
+    virtual int32_t addInput(Table* data, int32_t rowCount) = 0;
+    // orderby needs an array to sort
+    virtual int32_t addInput(Table** data, int32_t* rowCount, int32_t pageCount) = 0;
+    virtual int32_t getOutput(std::vector<Table*>& data) = 0;
+};
+
+#endif
