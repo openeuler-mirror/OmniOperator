05cb4f79d92d3fa357d57db6dd1b54d36368630f
liuyu <liuyu3@huawei.com>
2021-09-16 17:47:43 +0800

    do not cache hash builder operator factory

--- bindings/java/src/main/java/nova/hetu/omniruntime/operator/OmniOperatorFactoryContext.java
+++ bindings/java/src/main/java/nova/hetu/omniruntime/operator/OmniOperatorFactoryContext.java
@@ -10,6 +10,29 @@ package nova.hetu.omniruntime.operator;
  * @since 20210630
  */
 public abstract class OmniOperatorFactoryContext {
+    /**
+     * Whether the omni operator factory needs to be cached.
+     */
+    private boolean needCache = true;
+
+    /**
+     * Get the flag needCache whether the omni operator factory needs to be cached.
+     *
+     * @return the flag needCache
+     */
+    public boolean isNeedCache() {
+        return needCache;
+    }
+
+    /**
+     * Set the flag needCache whether the omni operator factory needs to be cached.
+     *
+     * @param needCache the flag needCache
+     */
+    public void setNeedCache(boolean needCache) {
+        this.needCache = needCache;
+    }
+
     @Override
     public int hashCode() {
         throw new RuntimeException("Unsupported hashCode");
