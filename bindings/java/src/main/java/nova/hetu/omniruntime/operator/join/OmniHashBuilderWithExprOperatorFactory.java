39459b0cbca70395ef08d09098759ab06c424aa2
c00516945 <caicongzhi@huawei.com>
2023-07-20 10:52:33 +0800

    bhj share hashtable

--- bindings/java/src/main/java/nova/hetu/omniruntime/operator/join/OmniHashBuilderWithExprOperatorFactory.java
+++ bindings/java/src/main/java/nova/hetu/omniruntime/operator/join/OmniHashBuilderWithExprOperatorFactory.java
@@ -6,6 +6,7 @@ package nova.hetu.omniruntime.operator.join;
 
 import static java.util.Objects.requireNonNull;
 
+import nova.hetu.omniruntime.operator.OmniOperator;
 import nova.hetu.omniruntime.operator.OmniOperatorFactory;
 import nova.hetu.omniruntime.operator.OmniOperatorFactoryContext;
 import nova.hetu.omniruntime.operator.config.OperatorConfig;
@@ -13,8 +14,13 @@ import nova.hetu.omniruntime.type.DataType;
 import nova.hetu.omniruntime.type.DataTypeSerializer;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * The Omni hash builder with expression operator factory.
@@ -23,6 +29,27 @@ import java.util.Optional;
  */
 public class OmniHashBuilderWithExprOperatorFactory
         extends OmniOperatorFactory<OmniHashBuilderWithExprOperatorFactory.FactoryContext> {
+    /**
+     * The global lock is used for sharing hash builder operator and factory concurrently.
+     */
+    public static final Lock gLock = new ReentrantLock();
+
+    /**
+     * The hashmap cached the shared hash builder operator, the key is the build plan node id.
+     */
+    private static Map<Integer, OmniOperator> operatorCache = new HashMap<>();
+
+    /**
+     * The hashmap cached the shared hash builder operator factory, the key is the build plan node id.
+     */
+    private static Map<Integer, OmniHashBuilderWithExprOperatorFactory> factoryCache = new HashMap<>();
+
+    /**
+     * The hashmap stores the num of lookup which is in-use shared hash builder operator
+     * the key is the build plan node id.
+     */
+    private static Map<Integer, AtomicInteger> ref = new HashMap<>();
+
     /**
      * Instantiates a new Omni hash builder with expression operator factory.
      *
@@ -59,6 +86,47 @@ public class OmniHashBuilderWithExprOperatorFactory
         return createHashBuilderWithExprOperatorFactory(DataTypeSerializer.serialize(context.buildTypes),
                 context.buildHashKeys, context.filterExpression, context.operatorCount,
                 OperatorConfig.serialize(context.operatorConfig));
+    }
+
+    /**
+     * save a new shared hash builder operator and factory into cache
+     *
+     * @param builderNodeId the build plan node id
+     * @param factory the shared hash builder operator factory
+     * @param operator the shared hash builder operator
+     */
+    public static void saveHashBuilderOperatorAndFactory(Integer builderNodeId,
+                                                         OmniHashBuilderWithExprOperatorFactory factory,
+                                                         OmniOperator operator) {
+        operatorCache.put(builderNodeId, operator);
+        factoryCache.put(builderNodeId, factory);
+    }
+
+    /**
+     * try get a shared hash builder factory form cache
+     *
+     * @param builderNodeId the build plan node id
+     * @return the shared hash builder operator factory
+     */
+    public static OmniHashBuilderWithExprOperatorFactory getHashBuilderOperatorFactory(Integer builderNodeId) {
+        ref.computeIfAbsent(builderNodeId, key -> new AtomicInteger(0));
+        ref.get(builderNodeId).incrementAndGet();
+        return factoryCache.get(builderNodeId);
+    }
+
+    /**
+     * try close and remove a shared hash builder factory form cache
+     *
+     * @param builderNodeId the build plan node id
+     */
+    public static void dereferenceHashBuilderOperatorAndFactory(Integer builderNodeId) {
+        if (ref.get(builderNodeId).decrementAndGet() == 0) {
+            ref.remove(builderNodeId);
+            operatorCache.get(builderNodeId).close();
+            operatorCache.remove(builderNodeId);
+            factoryCache.get(builderNodeId).close();
+            factoryCache.remove(builderNodeId);
+        }
     }
 
     /**
