5bad113046eef232bc9dbb3453dde85fd0226f4f
l30065184 <lijiahao71@h-partners.com>
2025-05-29 17:08:07 +0800

    update

--- core/src/operator/join/lookup_outer_join.cpp
+++ core/src/operator/join/lookup_outer_join.cpp
@@ -68,23 +68,18 @@ Operator *LookupOuterJoinOperatorFactory::CreateOperator()
 
 void LookupOuterJoinOperator::PrepareTotalVisitedCounts()
 {
-    std::visit(
-        [&](auto &&arg) {
-            size_t partitionIndex = 0;
-            while (partitionIndex < arg.GetHashTableSize()) {
-                if (arg.GetHashTableTypes(partitionIndex) == HashTableImplementationType::ARRAY_HASH_TABLE) {
-                    auto &hashTable = arg.GetArrayTable(partitionIndex);
-                    hashTable->ForEachValue(
-                        [&](const auto &value, const auto &index) { arg.SetTotalVisitedCounts(value->GetRowCount()); });
-                } else {
-                    auto &hashTable = arg.GetHashTable(partitionIndex);
-                    hashTable->hashmap.ForEachValue(
-                        [&](const auto &value, const auto &index) { arg.SetTotalVisitedCounts(value->GetRowCount()); });
-                }
-                partitionIndex++;
+    std::visit([&](auto &&arg) {
+        size_t partitionIndex = 0;
+        while (partitionIndex < arg.GetHashTableSize()) {
+            if (arg.GetHashTableTypes(partitionIndex) == HashTableImplementationType::ARRAY_HASH_TABLE) {
+                auto &hashTable = arg.GetArrayTable(partitionIndex);
+                hashTable->ForEachValue([&](const auto &value, const auto &index) { arg.SetTotalVisitedCounts(value->GetRowCount()); });
+            } else {
+                auto &hashTable = arg.GetHashTable(partitionIndex);
+                hashTable->hashmap.ForEachValue([&](const auto &value, const auto &index) { arg.SetTotalVisitedCounts(value->GetRowCount()); });
             }
-        },
-        *hashTables);
+            partitionIndex++;
+        }}, *hashTables);
 }
 
 LookupOuterJoinOperator::LookupOuterJoinOperator(DataTypes &probeOutputTypes, std::vector<int32_t> &probeOutputCols,
