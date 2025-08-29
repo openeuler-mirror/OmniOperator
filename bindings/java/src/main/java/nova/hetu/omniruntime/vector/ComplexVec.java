1e6ba667e8e95cc98c576ff7899d1ace211eaaf9
zhousipei <zhousipei@huawei.com>
2025-08-29 18:06:52 +0800

    add StructVec

---
+++ bindings/java/src/main/java/nova/hetu/omniruntime/vector/ComplexVec.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+ */
+
+package nova.hetu.omniruntime.vector;
+
+import nova.hetu.omniruntime.type.DataType;
+
+/**
+ * base class of complex vec.
+ *
+ * @since 2025-08-29
+ */
+public abstract class ComplexVec extends Vec {
+
+    public ComplexVec(long nativeVector, int capacityInBytes, int size, DataType dataType) {
+        super(nativeVector, getValuesNative(nativeVector), getValueNullsNative(nativeVector), capacityInBytes, size,
+            dataType);
+    }
+
+    protected static native int getComplexCapacityNative(long nativeVector, int vecEncodingId);
+
+    protected static native long newComplexVectorNative(int size, int vecEncodingId, DataType[] dataType);
+}
+
+
+
