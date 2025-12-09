183c0131c881e56f4887623d62cb966214c3bf79
向欣伟 <xiangxinwei@h-partners.com>
2025-12-09 11:32:55 +0000

    !23 fix_isNull * fix_isNull

--- core/src/vectorization/functions/IsNull.cpp
+++ core/src/vectorization/functions/IsNull.cpp
@@ -5,6 +5,7 @@
 #include "IsNull.h"
 #include "vector/vector.h"
 #include "../VectorFunction.h"
+#include "vectorization/SelectivityVector.h"
 
 namespace omniruntime::vectorization {
 using namespace omniruntime::vec;
@@ -19,17 +20,22 @@ public:
         args.pop();
         const auto raw = unsafe::UnsafeVector::GetRawValues(reinterpret_cast<Vector<bool> *>(result));
         const auto size = arg->GetSize();
-        for (int i = 0; i < size; i++) {
-            raw[i] = false;
-        }
+
+        memset(raw, 0, size * sizeof(bool));
+
+        const auto nullBits = reinterpret_cast<uint64_t *>(unsafe::UnsafeBaseVector::GetNulls(arg));
+        SelectivityVector rows(size);
+        rows.setFromBits(nullBits, size);
+        rows.applyToSelected([&](vector_size_t i) { raw[i] = true; });
     }
 };
 }
 
 void RegisterIsNullFunction(const std::string &name)
 {
-    std::vector<DataTypeId> param = {OMNI_INT};
-    auto signature = std::make_shared<codegen::FunctionSignature>(name, param, OMNI_BOOLEAN);
-    VectorFunction::functionMap_.insert(std::make_pair(signature, std::make_shared<IsNullFunction>()));
+    auto signatures = IsNullSignatures(name);
+    for (const auto &signature : signatures) {
+        VectorFunction::functionMap_.insert(std::make_pair(signature, std::make_shared<IsNullFunction>()));
+    }
 }
 }
