66a9aa2328173422f947c3b0f40042f0f5dc39c1
chenyuhan <chenyuhan16@huawei.com>
2026-01-20 16:39:20 +0800

    Revert "fixme: substitute coverage-C++ with release"
    This reverts commit cfe12a9cae061394d9e24dc6e3356d3444995d92.

--- build.sh
+++ build.sh
@@ -77,12 +77,14 @@ case "$1" in
     ;;
   coverage-c++)
     setup_dependencies release
+    echo "-- Enable coverage for c++"
+    cd ${CWD} && build coverage:java 
+    $CWD/build/core/test/omtest --gtest_output=xml:${CWD}/core/build/test_detail.xml
 
-    echo "-- Only build"
-    cd ${CWD} && build release:java --exclude-test
+    lcov --d $CWD/build --c --output-file test.info --rc lcov_branch_coverage=1
+    lcov --remove test.info '*/opt/buildtools/include/*' '*/usr/include/*' '*/usr/lib/*' '*/usr/lib64/*' '*/usr/local/include/*' '*/usr/local/lib/*' '*/usr/local/lib64/*' '*/test/*' -o final.info --rc lcov_branch_coverage=1
+    genhtml final.info -o ${CWD}/core/build/test_coverage --branch-coverage --rc lcov_branch_coverage=1
 
-    cd $CWD/bindings/java && mvn clean install -Domni.home=$OMNI_HOME -DskipTests
-    cd $CWD/core/src/udf/java && mvn clean install -DskipTests
     ;;
   coverage)
     setup_dependencies package
