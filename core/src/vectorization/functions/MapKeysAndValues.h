9d601d41519e913f21bdeb2fcac63f8a8b4200bc
陈健男 <1187914259@qq.com>
2026-01-26 09:48:31 +0800

    Add expr map_keys and map_values

---
+++ core/src/vectorization/functions/MapKeysAndValues.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ * Description: visitor class for expressions
+ */
+
+#pragma once
+#include "vectorization/VectorFunction.h"
+#include "vectorization/ExprEval.h"
+#include "vector/array_vector.h"
+#include "type/data_operations.h"
+#include "util/debug.h"
+#include <vector>
+#include <string_view>
+
+namespace omniruntime::vectorization {
+    using namespace omniruntime::type;
+    using namespace omniruntime::vec;
+    using namespace omniruntime::op;
+
+    template <bool isKeys>
+    class MapKeysAndValuesFunction : public VectorFunction {
+    public:
+        explicit MapKeysAndValuesFunction () {}
+
+        void Apply(std::stack<BaseVector *> &args, const DataTypePtr &outputType, BaseVector *&result,
+                   ExecutionContext *context) const override
+        {
+            BaseVector* inputVec = args.top();
+            args.pop();
+
+            if (inputVec == nullptr) {
+                OMNI_THROW("MapKeysAndValues Error:", "MapKeysAndValues received null vector argument");
+            }
+
+            auto* srcMapVec = dynamic_cast<MapVector*>(inputVec);
+            if (srcMapVec == nullptr) {
+                OMNI_THROW("MapKeysAndValues Error:", "MapKeysAndValues arguments must be Map type");
+            }
+
+            auto keyElements = srcMapVec->GetKeyVector();
+            auto valueElements = srcMapVec->GetValueVector();
+
+            int32_t totalRows = inputVec->GetSize();
+            result = new ArrayVector(totalRows);
+            auto *dstArrayVec = dynamic_cast<ArrayVector *>(result);
+
+            if (isKeys) {
+                dstArrayVec->SetElementVector(std::move(keyElements));
+            } else {
+                dstArrayVec->SetElementVector(std::move(valueElements));
+            }
+            for (int i = 0; i < totalRows; ++i) {
+                dstArrayVec->SetOffset(i+1, srcMapVec->GetOffset(i+1));
+                if (srcMapVec->IsNull(i)) {
+                    dstArrayVec->SetNull(i);
+                    continue;
+                }
+            }
+        }
+    };
+}
