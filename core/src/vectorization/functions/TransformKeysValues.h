0a7bb0c3ef84c11e45e4c33a293b7e0b11f64887
陈健男 <1187914259@qq.com>
2026-02-02 19:40:22 +0800

    Add expr transform_keys and transform_values

---
+++ core/src/vectorization/functions/TransformKeysValues.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ * Description: visitor class for expressions
+ */
+
+#pragma once
+#include "vectorization/VectorFunction.h"
+#include "vectorization/ExprEval.h"
+#include "type/data_operations.h"
+#include "util/debug.h"
+#include <vector>
+#include <string_view>
+
+namespace omniruntime::vectorization {
+    using namespace omniruntime::type;
+    using namespace omniruntime::vec;
+    using namespace omniruntime::op;
+
+    template <bool isKeys>
+    class TransformKeysValuesVectorFunction : public VectorFunction {
+    public:
+        explicit TransformKeysValuesVectorFunction () {}
+
+        void Apply(std::stack<BaseVector *> &args, const DataTypePtr &outputType, BaseVector *&result,
+                   ExecutionContext *context) const override
+        {
+            BaseVector *arrBaseVec = args.top();
+            args.pop();
+
+            const expressions::LambdaExpr *lambdaExpr = context->GetCurrentLambda();
+            if (lambdaExpr == nullptr) {
+                throw OmniException("TRANSFORM_KEYS_VALUES_ERROR", "lambdaExpr is null");
+            }
+            expressions::Expr *lambdaBody = lambdaExpr->GetBody();
+            if (lambdaBody->GetType() == expressions::ExprType::PARAM_REF_E) {
+                result = arrBaseVec;
+                return;
+            }
+            int32_t mapRowSize = arrBaseVec->GetSize();
+            MapVector *srcMapVec = dynamic_cast<MapVector*>(arrBaseVec);
+            if (!srcMapVec) {
+                OMNI_THROW("TRANSFORM_KEYS_VALUES_ERROR", "Input vector is not a valid MapVector");
+            }
+            auto srcKeyEleTypeId = srcMapVec->GetKeyVector()->GetTypeId();
+            auto srcValueEleTypeId = srcMapVec->GetValueVector()->GetTypeId();
+
+            result = new MapVector(mapRowSize);
+            auto *dstMapVec = static_cast<MapVector *>(result);
+
+            for (int32_t i = 0; i < mapRowSize; ++i) {
+                if (srcMapVec->IsNull(i)) {
+                    dstMapVec->SetNull(i);
+                }
+                dstMapVec->SetOffset(i+1,srcMapVec->GetOffset(i+1));
+            }
+
+            if (mapRowSize == 0 || srcMapVec->GetNullCount() == mapRowSize) {
+                VectorHelper::EmptyMapProjection(dstMapVec, srcKeyEleTypeId, srcValueEleTypeId);
+                return;
+            }
+
+            BaseVector *keyElementVec = nullptr;
+            BaseVector *valueElementVec = nullptr;
+            keyElementVec = srcMapVec->GetKeyVector().get();
+            valueElementVec = srcMapVec->GetValueVector().get();
+            if (keyElementVec == nullptr || keyElementVec->GetSize() == 0 ||
+                valueElementVec == nullptr || valueElementVec->GetSize() == 0) {
+                VectorHelper::EmptyMapProjection(dstMapVec, srcKeyEleTypeId, srcValueEleTypeId);
+                return;
+            }
+            keyElementVec->SetIsField(true);
+            valueElementVec->SetIsField(true);
+
+            ExprEval lambdaEval(context);
+            lambdaEval.paramNameToIdxMap = lambdaExpr->paramNameToIdxMap_;
+            lambdaEval.lambdaParams_.push_back(keyElementVec);
+            lambdaEval.lambdaParams_.push_back(valueElementVec);
+
+            context->SetResultRowSize(keyElementVec->GetSize());
+            lambdaBody->Accept(lambdaEval);
+            context->SetResultRowSize(mapRowSize);
+            BaseVector *flatResultVec = lambdaEval.GetResult();
+            if (flatResultVec == nullptr) {
+                throw OmniException("TRANSFORM_ERROR", "Lambda execute return null result");
+            }
+
+            if (isKeys) {
+                dstMapVec->SetValueVector(srcMapVec->GetValueVector());
+                dstMapVec->AddKeys(flatResultVec);
+            } else {
+                dstMapVec->SetKeyVector(srcMapVec->GetKeyVector());
+                dstMapVec->AddValues(flatResultVec);
+            }
+            delete srcMapVec;
+        }
+    };
+}
--- core/src/vectorization/registration/RegisterLambda.cpp
+++ core/src/vectorization/registration/RegisterLambda.cpp
@@ -5,27 +5,63 @@
 
 #include "RegistrationHelpers.h"
 #include "vectorization/functions/TransForm.h"
+#include "vectorization/functions/TransformKeysValues.h"
 
 namespace omniruntime::vectorization {
+
+    static constexpr std::array<DataTypeId, 9> BASE_DATA_TYPES = {
+            OMNI_INT,
+            OMNI_LONG,
+            OMNI_DOUBLE,
+            OMNI_BOOLEAN,
+            OMNI_SHORT,
+            OMNI_VARCHAR,
+            OMNI_CHAR,
+            OMNI_BYTE,
+            OMNI_FLOAT
+    };
+
+    template <typename FuncT>
+    void RegisterTransformSingleType(const std::string& funcName, DataTypeId elemType, const std::shared_ptr<FuncT>& func) {
+        VectorFunction::RegisterVectorFunction(
+                funcName,
+                {OMNI_ARRAY, elemType},
+                OMNI_ARRAY,
+                func
+        );
+    }
+
+    template <typename FuncT>
+    void RegisterMapTransformSingleType(const std::string& funcName, DataTypeId elemType, const std::shared_ptr<FuncT>& func) {
+        VectorFunction::RegisterVectorFunction(
+                funcName,
+                {OMNI_MAP, elemType},
+                OMNI_MAP,
+                func
+        );
+    }
+
+    template <typename F, size_t N>
+    void BatchRegister(const std::array<DataTypeId, N>& types, F&& f) {
+        (f(types[0]), f(types[1]), f(types[2]), f(types[3]), f(types[4]),
+                f(types[5]), f(types[6]), f(types[7]), f(types[8]));
+    }
+
     void RegisterLambdaFunctions(const std::string &prefix)
     {
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_INT}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_LONG}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_DOUBLE}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_BOOLEAN}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_SHORT}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_VARCHAR}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_CHAR}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_BYTE}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
-        VectorFunction::RegisterVectorFunction("transform", {OMNI_ARRAY, OMNI_FLOAT}, OMNI_ARRAY,
-            std::make_shared<TransformVectorFunction>());
+        auto transformFunc = std::make_shared<TransformVectorFunction>();
+        BatchRegister(BASE_DATA_TYPES, [&](DataTypeId elemType) {
+            RegisterTransformSingleType("transform", elemType, transformFunc);
+        });
+
+        auto transformKeysFunc = std::make_shared<TransformKeysValuesVectorFunction<true>>();
+        BatchRegister(BASE_DATA_TYPES, [&](DataTypeId elemType) {
+            RegisterMapTransformSingleType("transform_keys", elemType, transformKeysFunc);
+        });
+
+        auto transformValuesFunc = std::make_shared<TransformKeysValuesVectorFunction<false>>();
+        BatchRegister(BASE_DATA_TYPES, [&](DataTypeId elemType) {
+            RegisterMapTransformSingleType("transform_values", elemType, transformValuesFunc);
+        });
     }
 }
