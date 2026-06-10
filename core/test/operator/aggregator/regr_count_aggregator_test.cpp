77541807bc0b2907caff67e8f5e0bf03f092fa5e
Nineee999 <906992927@qq.com>
2026-06-10 10:17:04 +0800

    fix: fix regr align schema for partial skip feature

--- core/test/operator/aggregator/regr_count_aggregator_test.cpp
+++ core/test/operator/aggregator/regr_count_aggregator_test.cpp
@@ -115,7 +115,7 @@ TEST(RegrCountAggregatorTest, AlignAggSchemaRawOneRow)
 {
     DataTypes inD({DoubleType(), DoubleType()});
     DataTypes outPartial(
-        {LongType(), DoubleType(), DoubleType(), DoubleType(), DoubleType(), DoubleType()});
+        {DoubleType(), DoubleType(), DoubleType(), DoubleType(), DoubleType(), DoubleType()});
     std::vector<int32_t> ch = {0, 1};
     auto agg = CreateRegrAggregatorForUt(OMNI_AGGREGATION_TYPE_REGR_COUNT, inD, outPartial, ch, true, true);
     auto *batchIn = new VectorBatch(1);
@@ -127,7 +127,7 @@ TEST(RegrCountAggregatorTest, AlignAggSchemaRawOneRow)
     batchIn->Append(xv);
     auto *batchOut = new VectorBatch(0);
     agg->AlignAggSchema(batchOut, batchIn);
-    EXPECT_EQ(static_cast<Vector<int64_t> *>(batchOut->Get(0))->GetValue(0), 1);
+    EXPECT_DOUBLE_EQ(static_cast<Vector<double> *>(batchOut->Get(0))->GetValue(0), 1.0);
     EXPECT_DOUBLE_EQ(static_cast<Vector<double> *>(batchOut->Get(1))->GetValue(0), 2.0);
     EXPECT_DOUBLE_EQ(static_cast<Vector<double> *>(batchOut->Get(2))->GetValue(0), 5.0);
     EXPECT_DOUBLE_EQ(static_cast<Vector<double> *>(batchOut->Get(3))->GetValue(0), 0.0);
